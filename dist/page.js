3409b9cf66558188a155684ee64020a28a3c334c
Kian Musser <47985354+Kian8017@users.noreply.github.com>
2021-01-09 00:32:39 -0600

    Added embeddedDocs for blurbs!

--- dist/page.js
+++ dist/page.js
@@ -1,6 +1,7 @@
 import {h} from "../web_modules/preact.js";
 import Section from "./section.js";
 import Block from "./block.js";
+import EmbeddedDoc from "./embedded_doc.js";
 import "./page.css.proxy.js";
 function Page() {
   return /* @__PURE__ */ h("div", {
@@ -29,14 +30,20 @@ function Page() {
     abstract: "Family Home Evening Info"
   }, /* @__PURE__ */ h(Section, {
     abstract: "Cameron"
-  }), /* @__PURE__ */ h(Section, {
-    abstract: "Gallatin"
-  }), /* @__PURE__ */ h(Section, {
+  }, /* @__PURE__ */ h(EmbeddedDoc, {
+    url: "https://docs.google.com/document/d/e/2PACX-1vQXWa6XY49byuvrsZdYVRcGRJy2USSNV57yoq6fx32dongeY997xoHKMnfLZW5TXRN3khWAcDFDm_S_/pub?embedded=true"
+  })), /* @__PURE__ */ h(Section, {
     abstract: "Kearney"
-  }), /* @__PURE__ */ h(Section, {
+  }, /* @__PURE__ */ h(EmbeddedDoc, {
+    url: "https://docs.google.com/document/d/e/2PACX-1vQtXuJiK-KBxynhfjc13rEduxAHcV4fcXqKMZmm0U_boMKKdov--AwcbMr1hj6XxZOKY5aBJgfDM7Qf/pub?embedded=true"
+  })), /* @__PURE__ */ h(Section, {
     abstract: "Maryville"
-  }), /* @__PURE__ */ h(Section, {
+  }, /* @__PURE__ */ h(EmbeddedDoc, {
+    url: "https://docs.google.com/document/d/e/2PACX-1vR5FqLon16N6MjhPxsSWHwjRnbTY0YBcMv-oaAH64D92eVcC8pV-Ug0Me1-yuJsdXnbW97OjU_K1p2o/pub?embedded=true"
+  })), /* @__PURE__ */ h(Section, {
     abstract: "Virtual"
-  })));
+  }, /* @__PURE__ */ h(EmbeddedDoc, {
+    url: "https://docs.google.com/document/d/e/2PACX-1vTjx8C22yQY-st19rOoYv9lXK_XklxodDlsXzVja24NIfwVS5lzYhxNOB9ftY4Zzh6sDOFzQfcpNM6Z/pub?embedded=true"
+  }))));
 }
 export default Page;
