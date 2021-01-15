580f228fe955efc016e0df5158b5ece7ace95579
Kian Musser <47985354+Kian8017@users.noreply.github.com>
2021-01-14 20:52:47 -0600

    Added better bio

--- dist/widget/bio.css.proxy.js
+++ dist/widget/bio.css.proxy.js
@@ -1,6 +1,6 @@
 // [snowpack] add styles to the page (skip if no document exists)
 if (typeof document !== 'undefined') {
-  const code = ".bio {\n\tdisplay: flex;\n\tflex-direction: row;\n\tjustify-content: space-between;\n}\n\n.bioinfo {\n\tflex-grow: 1;\n\tdisplay: flex;\n\tflex-direction: column;\n}\n\n.bioinfo hr {\n\tmargin-left: 0;\n\tmargin-right: 0;\n}\n";
+  const code = ".bioabovefold {\n\tdisplay: flex;\n\tflex-direction: row;\n\tjustify-content: space-between;\n}\n\n.bioimg {\n\tmin-width: 100px;\n\tbackground-color: lightblue;\n}\n.bioinfo {\n\tmargin-left: 5px;\n\tflex-grow: 1;\n\tdisplay: flex;\n\tflex-direction: column;\n}\n\n.bioinfo hr {\n\tmargin-top: 0.2em;\n\tmargin-bottom: 0.1em;\n\tmargin-left: 0;\n\tmargin-right: 0;\n}\n\n.bioinfolower {\n\tfont-size: smaller;\n}\n\n.userdata {\n\tfont-weight: bold;\n}\n\n.biobelowfold {\n\n}\n";
 
   const styleEl = document.createElement("style");
   const codeEl = document.createTextNode(code);
--- dist/widget/bio.js
+++ dist/widget/bio.js
@@ -1,8 +1,19 @@
 import {h} from "../../web_modules/preact.js";
+import {useState} from "../../web_modules/preact/hooks.js";
 import "./bio.css.proxy.js";
 export default function Bio(props) {
+  const [showMore, setShowMore] = useState(false);
+  const clickHandler = () => {
+    setShowMore(!showMore);
+  };
+  const belowFold = /* @__PURE__ */ h("div", {
+    className: "biobelowfold"
+  });
   return /* @__PURE__ */ h("div", {
-    className: "whitebackground bio"
+    className: "whitebackground bio",
+    onClick: clickHandler
+  }, /* @__PURE__ */ h("div", {
+    className: "bioabovefold"
   }, /* @__PURE__ */ h("div", {
     className: "bioimg"
   }), /* @__PURE__ */ h("div", {
@@ -11,5 +22,11 @@ export default function Bio(props) {
     className: "bioinfoupper"
   }, /* @__PURE__ */ h("span", null, props.data.name)), /* @__PURE__ */ h("hr", null), /* @__PURE__ */ h("div", {
     className: "bioinfolower"
-  })));
+  }, /* @__PURE__ */ h("span", null, "Gender: ", /* @__PURE__ */ h("span", {
+    className: "userdata"
+  }, props.data.info.gender)), /* @__PURE__ */ h("br", null), /* @__PURE__ */ h("span", null, "Home Ward: ", /* @__PURE__ */ h("span", {
+    className: "userdata"
+  }, props.data.info.homeWard)), /* @__PURE__ */ h("br", null), /* @__PURE__ */ h("span", null, "Preferred Contact: ", /* @__PURE__ */ h("span", {
+    className: "userdata"
+  }, props.data.contact.preferred.value, " (", props.data.contact.preferred.type, ")"))))), showMore ? /* @__PURE__ */ h("hr", null) : null, showMore ? belowFold : null);
 }
