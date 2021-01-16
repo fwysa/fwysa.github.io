c63f71aaa2339361d0d951812a938ec5d9d35377
Kian Musser <47985354+Kian8017@users.noreply.github.com>
2021-01-16 14:13:18 -0600

    Added ward list

--- dist/constants.js
+++ dist/constants.js
@@ -2,7 +2,7 @@ const CONSTANTS = {
   genders: ["Male", "Female"],
   statuses: ["Unassigned", "Assigned", "Calling (in process)", "Contacted", "Not Interested", "Unable to Contact", "YSA Member"],
   fheGroups: ["Cameron", "Maryville", "Kearney", "Gallatin", "Virtual"],
-  homeWards: ["Grindstone Creek", "FIXME: Add home wards"],
+  homeWards: ["Crooked River Ward", "Far West Ward", "Far West YSA Ward", "Fishing River Ward", "Gallatin Ward", "Grand River Valley Ward", "Grindstone Creek Ward", "Kearney Ward", "Maryville Ward", "Spring Hill Ward", "Three Forks Ward", "Whitmer Ward", "Yellow Creek Branch"],
   contactMethods: ["Text", "Call", "Email"],
   sources: ["YSA Fellowship", "Church", "YSA Activity", "Home Evening", "Other"]
 };
