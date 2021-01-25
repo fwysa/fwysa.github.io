20a808d77d3e0b6d68a4cea0e42671327130b66d
Kian Musser <kianmusser+fwysa@gmail.com>
2021-01-25 16:27:08 -0600

    Fixed editabletextarea

--- dist/element/editable/textarea.js
+++ dist/element/editable/textarea.js
@@ -1,15 +1,17 @@
 import {h} from "../../../web_modules/preact.js";
+import {useUserProperty} from "../../db/hooks.js";
 export default function EditableText(props) {
+  const [value, setValue] = useUserProperty(props.name, props.property);
   const editChangeHandler = (e) => {
-    props.setcb(e.target.value);
+    setValue(e.target.value);
   };
   const normal = /* @__PURE__ */ h("span", null, /* @__PURE__ */ h("textarea", {
-    value: props.val,
+    value,
     placeholder: "Add useful info here",
     disabled: true
   }));
   const editing = /* @__PURE__ */ h("span", null, /* @__PURE__ */ h("textarea", {
-    value: props.val,
+    value,
     onChange: editChangeHandler,
     placeholder: "Add useful info here"
   }));
